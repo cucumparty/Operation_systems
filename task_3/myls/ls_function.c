1664a34e9f506ad7ec3cbf8e795f41c4e92cacbd
Rushana Denisheva <denisheva.rr@phystech.edu>
2022-10-05 20:20:34 +0300

    ..

--- task_3/myls/ls_function.c
+++ task_3/myls/ls_function.c
@@ -73,25 +73,25 @@ int flag_func(DIR* file, int argc, char** argv, int flag_a, int flag_l, int flag
 
                 switch(flag_a + flag_l + flag_R)
                 {
-                case 0: if(argc < 3)
-                                file = opendir(".");
-                        else
-                                file = opendir(argv[optind + 1]);
+                case 0: if(argc == 1)
+                              file = opendir(".");  
+                        if(argc == 2)
+                                file = opendir(argv[optind]);
                         break; 
-                case 1: if(argc < 4)
-                                file = opendir(".");
-                        else
-                                file = opendir(argv[optind + 1]);
+                case 1: if(argc == 2)
+                              file = opendir(".");  
+                        if(argc == 3)
+                                file = opendir(argv[optind]);
                         break;
-                case 2: if(argc < 5)
-                                file = opendir(".");
-                        else
-                                file = opendir(argv[optind + 1]);
+                case 2: if(argc == 3)
+                              file = opendir(".");  
+                        if(argc == 4)
+                                file = opendir(argv[optind]);
                         break;
-                case 3: if(argc < 6)
-                                file = opendir(".");
-                        else
-                                file = opendir(argv[optind + 1]);
+                case 3: if(argc == 4)
+                              file = opendir(".");  
+                        if(argc == 5)
+                                file = opendir(argv[optind]);
                         break;
                 }
 
